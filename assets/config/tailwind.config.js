daee3859ecf6e27a174bbc3ff67c494890a12667
Jun Woo Shin <shin.junwoo97@gmail.com>
2023-08-27 19:37:21 -0400

    [layout] copy postcss partial

---
+++ assets/config/tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+	darkMode: 'class',
+	content: [
+		"./themes/**/layouts/**/*.html",
+		"./content/**/layouts/**/*.html",
+		"./layouts/**/*.html",
+		"./content/**/*.html",
+		"./content/**/*.md"
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				'sans' : [
+					'"Poppins"',
+					'"Inter"',
+					'-apple-system',
+					'BlinkMacSystemFont',
+					'avenir next',
+					'avenir',
+					'segoe ui',
+					'helvetica neue',
+					'helvetica',
+					'Cantarell',
+					'Ubuntu',
+					'roboto',
+					'noto',
+					'arial',
+					'sans-serif'
+				],
+			},
+		},
+	},
+	plugins: [],
+	variants: ['group-hover'],
+};
